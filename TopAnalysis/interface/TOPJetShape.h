270758b834d222e8cdf7fca02644c6953cf99ec2
Markus Seidel <markus.seidel@cern.ch>
2016-05-27 20:17:15 +0200

    Reco level

--- TopAnalysis/interface/TOPJetShape.h
+++ TopAnalysis/interface/TOPJetShape.h
@@ -5,22 +5,26 @@
 
 struct TopJetShapeEvent_t
 {
-  Int_t cat,nw,nl,nj,nt,ngj,ngl;
-  Float_t weight[10];
-  Float_t l_pt[5],l_eta[5],l_phi[5],l_m[5];
-  Int_t l_id[5];
-  Float_t gl_pt[5],gl_eta[5],gl_phi[5],gl_m[5];
-  Int_t gl_id[5];
-  Float_t j_pt[50],j_eta[50],j_phi[50],j_m[50];
-  Float_t t_pt[4],t_eta[4],t_phi[4],t_m[4];
-  Int_t t_id[4];
-  Float_t met_pt,met_phi;
+  Int_t nw, nl, ngl, nj, ngj;
+  Int_t gen_sel, reco_sel;
   
-  Int_t gen_sel;
-  Float_t gj_pt[50],gj_eta[50],gj_phi[50],gj_m[50];
-  Int_t gj_flav[50],gj_hadflav[50];
+  Float_t weight[10];
+  
+  Float_t l_pt[5], l_eta[5], l_phi[5], l_m[5];
+  Int_t l_id[5];
+  
+  Float_t gl_pt[5], gl_eta[5], gl_phi[5], gl_m[5];
+  Int_t gl_id[5];
+  
+  Float_t j_pt[50], j_eta[50], j_phi[50], j_m[50];
+  Int_t j_flav[50], j_gj[50];
+  Float_t j_ga[50][3][3][3][3];
+  
+  Float_t gj_pt[50], gj_eta[50], gj_phi[50], gj_m[50];
+  Int_t gj_flav[50], gj_j[50];
   Float_t gj_ga[50][3][3][3][3];
   
+  Float_t met_pt,met_phi;
 };
 
 struct Particle {
@@ -43,9 +47,6 @@ struct Jet {
   Jet(TLorentzVector p, int f, int oi)
   : p4(p), flav(f), oldidx(oi) {}
 };
-
-enum {charged, all, puppi};
-enum {pt0, pt500, pt1000};
 
 double calcGA(Jet jet, int beta, int kappa, int iptcut, int icharge);
 void createTopJetShapeEventTree(TTree *t,TopJetShapeEvent_t &tjsev);
--- TopAnalysis/src/TOPJetShape.cc
+++ TopAnalysis/src/TOPJetShape.cc
@@ -170,7 +170,7 @@ void RunTopJetShape(TString filename,
   for (auto& it : allPlots)   { it.second->Sumw2(); it.second->SetDirectory(0); }
   for (auto& it : all2dPlots) { it.second->Sumw2(); it.second->SetDirectory(0); }
 
-  nentries = 100000;
+  nentries = 10000;
   //LOOP OVER EVENTS
   for (Int_t iev=0;iev<nentries;iev++)
     {
@@ -178,12 +178,11 @@ void RunTopJetShape(TString filename,
       resetTopJetShapeEvent(tjsev);
       if(iev%100==0) printf ("\r [%3.0f/100] done",100.*(float)(iev)/(float)(nentries));
       
-      //GENERATOR LEVEL
+      //GENERATOR LEVEL SELECTION
       
       int sel_ngleptons = 0;
       int vet_ngleptons = 0;
       int sel_ngbjets   = 0;
-      int sel_ngjets    = 0;
       int sel_ngwcand   = 0;
       std::vector<Jet> genJets;
       
@@ -196,8 +195,7 @@ void RunTopJetShape(TString filename,
           genJets.push_back(Jet(jp4, abs(ev.g_id[i]), i));
           //count
           tjsev.ngj++;
-          if      (abs(ev.g_id[i]) <5) sel_ngjets++;
-          else if (abs(ev.g_id[i])==5) sel_ngbjets++;
+          if (abs(ev.g_id[i])==5) sel_ngbjets++;
         }
         //leptons
         else if (abs(ev.g_id[i])>10) {
@@ -216,12 +214,13 @@ void RunTopJetShape(TString filename,
       }
       
       //flag non-b jets as part of W boson candidates: flav 0->1
+      //TODO: matched events at 8 TeV: mu=84.23, sigma=12.39 GeV. Correction needed?
       for (int i = 0; i < tjsev.ngj; i++) {
         if (genJets[i].flav==5) continue;
         for (int j = i+1; j < tjsev.ngj; j++) {
           if (genJets[j].flav==5) continue;
           TLorentzVector wCand = genJets[i].p4 + genJets[j].p4;
-          if (abs(wCand.M()-80.4) < 10.) {
+          if (abs(wCand.M()-80.4) < 15.) {
             genJets[i].flav = 1;
             genJets[j].flav = 1;
             sel_ngwcand++;
@@ -233,35 +232,13 @@ void RunTopJetShape(TString filename,
       if (sel_ngbjets==2 && sel_ngwcand>0 && 
           sel_ngleptons==1 && vet_ngleptons==0) tjsev.gen_sel = 1;
       
-      //fill jet constituents
-      for (int i = 0; i < tjsev.ngj; i++) {
-        int idx = genJets[i].oldidx;
-        for (int p = 0; p < ev.ngpf; p++) {
-          if (ev.gpf_g[p] == idx) {
-            TLorentzVector pp4;
-            pp4.SetPtEtaPhiM(ev.gpf_pt[p],ev.gpf_eta[p],ev.gpf_phi[p],ev.gpf_m[p]);
-            genJets[i].particles.push_back(Particle(pp4, ev.gpf_c[p], 1.));
-          }
-        }
-      }
-      
-      //calculate generalized angularities (jet shapes)
-      for (int i = 0; i < tjsev.ngj; i++) {
-        for (int i1 = 0; i1 < 3; ++i1) for (int i2 = 0; i2 < 3; ++i2) for (int i3 = 0; i3 < 3; ++i3) for (int i4 = 0; i4 < 3; ++i4)
-        tjsev.gj_ga[i][i1][i2][i3][i4] = calcGA(genJets[i], i1, i2, i3, i4);
-      }
-      
-      //store analyzed jets to tree
-      for (int i = 0; i < tjsev.ngj; i++) {
-        tjsev.gj_pt  [i] = genJets[i].p4.Pt();
-        tjsev.gj_eta [i] = genJets[i].p4.Eta();
-        tjsev.gj_phi [i] = genJets[i].p4.Phi();
-        tjsev.gj_m   [i] = genJets[i].p4.M();
-        tjsev.gj_flav[i] = genJets[i].flav;
-      }
 
-      //RECO LEVEL
-      /*
+      //RECO LEVEL SELECTION
+      
+      int sel_nbjets = 0;
+      int sel_nwcand = 0;
+      std::vector<Jet> jets;
+      
       //account for pu weights and effect on normalization
       float puWeight(1.0);
       if(!ev.isData) 
@@ -290,11 +267,17 @@ void RunTopJetShape(TString filename,
       bool hasEleTrigger((ev.elTrigger & 0x1)!=0);
 
       //decide the channel
-      TString chTag("");
       std::vector<int> selLeptons;
+      selLeptons.insert(selLeptons.end(), tightLeptons.begin(), tightLeptons.end());
+      selLeptons.insert(selLeptons.end(), looseLeptons.begin(), looseLeptons.end());
+      /*
       if(tightLeptons.size()==1 && looseLeptons.size()==0)
         {
           selLeptons.push_back( tightLeptons[0] );
+        }
+      if(tightLeptons.size()==0 && looseLeptons.size()==1)
+        {
+          selLeptons.push_back( looseLeptons[0] );
         }
       if(tightLeptons.size()>=2)
         {
@@ -306,28 +289,7 @@ void RunTopJetShape(TString filename,
           selLeptons.push_back(tightLeptons[0]);
           selLeptons.push_back(looseLeptons[0]);          
         }
-      if(selLeptons.size()==0) continue;
-      if(selLeptons.size()==1)
-        {
-          if(abs(ev.l_id[ selLeptons[0] ])==11 && hasEleTrigger) chTag="E";
-          if(abs(ev.l_id[ selLeptons[0] ])==13 && hasMuTrigger)  chTag="M";
-        }
-      if(selLeptons.size()==2)
-        {
-          if(abs(ev.l_id[ selLeptons[0] ]*ev.l_id[ selLeptons[1] ])==11*11 && hasEleTrigger) chTag="EE";
-          if(abs(ev.l_id[ selLeptons[0] ]*ev.l_id[ selLeptons[1] ])==13*13 && hasMuTrigger) chTag="MM";
-          if(abs(ev.l_id[ selLeptons[0] ]*ev.l_id[ selLeptons[1] ])==11*13)
-            {
-              if(tightLeptons.size()>=2 && (hasEleTrigger || hasMuTrigger)) chTag="EM";
-              if(tightLeptons.size()==1)
-                {
-                  if(abs(ev.l_id[ selLeptons[0] ])==11 && hasEleTrigger) chTag="EM";
-                  if(abs(ev.l_id[ selLeptons[0] ])==13 && hasMuTrigger) chTag="EM";
-                }
-            }
-          if(hasMuTrigger && requireEtriggerOnly) chTag="";
-        }
-      if(chTag=="") continue;
+      */
 
       //save lepton kinematics
       std::vector<TLorentzVector> leptons;
@@ -340,8 +302,6 @@ void RunTopJetShape(TString filename,
         }
 
       //select jets
-      std::vector<int> genBJetsFlav,genOtherjetsFlav,genBJetsHadFlav,genOtherjetsHadFlav;
-      std::vector<TLorentzVector> bJets,otherjets, genBJets, genOtherjets;
       for (int k=0; k<ev.nj;k++)
         {
           //check kinematics
@@ -365,7 +325,10 @@ void RunTopJetShape(TString filename,
               float jerSmear=getJetResolutionScales(jp4.Pt(),jp4.Pt(),genJet_pt)[0];
               jp4 *= jerSmear;
             }
-
+          
+          //jet kinematic selection
+          if(jp4.Pt() < 30 || abs(jp4.Eta()) > 2.4) continue;
+          
           //b-tag
           float csv = ev.j_csv[k];          
           bool isBTagged(csv>0.800);
@@ -396,28 +359,69 @@ void RunTopJetShape(TString filename,
               myBTagSFUtil.modifyBTagsWithSF(isBTagged,    jetBtagSF,     expEff);
             }
 
-          if(jp4.Pt()<30) continue;
-
-          Int_t hadFlav=ev.j_hadflav[k];
-          Int_t flav=ev.j_flav[k];
-          TLorentzVector gjp4(0,0,0,0);
-          if(ev.j_g[k]>-1)
-            {
-              int gidx=ev.j_g[k];
-              gjp4.SetPtEtaPhiM( ev.g_pt[gidx], ev.g_eta[gidx], ev.g_phi[gidx], ev.g_m[gidx] );
-            }
-
-          //save jets 
-          if(isBTagged && fabs(jp4.Eta()) < 2.4 && bJets.size()<2) 
-            { bJets.push_back(jp4);     genBJets.push_back(gjp4);     genBJetsFlav.push_back(flav);     genBJetsHadFlav.push_back(hadFlav);    }
-          else 
-            { otherjets.push_back(jp4); genOtherjets.push_back(gjp4); genOtherjetsFlav.push_back(flav); genOtherjetsHadFlav.push_back(hadFlav); }
+          //flavor based on b tagging
+          int flav = 0;
+          if (isBTagged) {
+            flav = 5;
+            sel_nbjets++;
+          }
+          
+          //store to jet analysis objects
+          jets.push_back(Jet(jp4, flav, k));
         }
+      
+      tjsev.nj=jets.size();
+      
+      //flag non-b jets as part of W boson candidates: flav 0->1
+      for (int i = 0; i < tjsev.nj; i++) {
+        if (jets[i].flav==5) continue;
+        for (int j = i+1; j < tjsev.nj; j++) {
+          if (jets[j].flav==5) continue;
+          TLorentzVector wCand = jets[i].p4 + jets[j].p4;
+          if (abs(wCand.M()-80.4) < 15.) {
+            jets[i].flav = 1;
+            jets[j].flav = 1;
+            sel_nwcand++;
+          }
+        }
+      }
+      
+      //event selected on reco level?
+      if (sel_nbjets==2 && sel_nwcand>0 && 
+          tightLeptons.size()==1 && looseLeptons.size()==0) tjsev.reco_sel = 1;
+      
+      //proceed only if event is selected on gen or reco level
+      if (tjsev.gen_sel + tjsev.reco_sel == -2) continue;
+      
+      
+      //GEN LEVEL ANALYSIS
+      
+      //fill jet constituents
+      for (int i = 0; i < tjsev.ngj; i++) {
+        int idx = genJets[i].oldidx;
+        for (int p = 0; p < ev.ngpf; p++) {
+          if (ev.gpf_g[p] == idx) {
+            TLorentzVector pp4;
+            pp4.SetPtEtaPhiM(ev.gpf_pt[p],ev.gpf_eta[p],ev.gpf_phi[p],ev.gpf_m[p]);
+            genJets[i].particles.push_back(Particle(pp4, ev.gpf_c[p], 1.));
+          }
+        }
+      }
+      
+      //store jets to tree, including generalized angularities (jet shapes)
+      for (int i = 0; i < tjsev.ngj; i++) {
+        tjsev.gj_pt  [i] = genJets[i].p4.Pt();
+        tjsev.gj_eta [i] = genJets[i].p4.Eta();
+        tjsev.gj_phi [i] = genJets[i].p4.Phi();
+        tjsev.gj_m   [i] = genJets[i].p4.M();
+        tjsev.gj_flav[i] = genJets[i].flav;
+        for (int i1 = 0; i1 < 3; ++i1) for (int i2 = 0; i2 < 3; ++i2) for (int i3 = 0; i3 < 3; ++i3) for (int i4 = 0; i4 < 3; ++i4)
+        tjsev.gj_ga[i][i1][i2][i3][i4] = calcGA(genJets[i], i1, i2, i3, i4);
+      }
 
-      //2 b-jets are required 
-      //notice more can be b-tagged but they are put into the other jets category
-      if(bJets.size()!=2) continue;
-
+      
+      //RECO LEVEL ANALYSIS
+      
       //event weight
       float wgt(1.0);
       if(!ev.isData)
@@ -452,100 +456,57 @@ void RunTopJetShape(TString filename,
         }
       
       //nominal selection control histograms
-      allPlots["nvtx_"+chTag]->Fill(ev.nvtx,wgt);
-      allPlots["njets_"+chTag]->Fill(otherjets.size(),wgt);
+      //TODO allPlots["nvtx_"+chTag]->Fill(ev.nvtx,wgt);
 
+      //fill leptons
       tjsev.nl=leptons.size();
       for(int il=0; il<(int)leptons.size(); il++)
         {
-          TString pf(Form("l%d",il));
-          allPlots[pf+"pt_"+chTag]->Fill(leptons[il].Pt(),wgt);
-          allPlots[pf+"eta_"+chTag]->Fill(fabs(leptons[il].Eta()),wgt);
           tjsev.l_pt[il]=leptons[il].Pt();
           tjsev.l_eta[il]=leptons[il].Eta();
           tjsev.l_phi[il]=leptons[il].Phi();
           tjsev.l_m[il]=leptons[il].M();
           tjsev.l_id[il]=ev.l_id[ selLeptons[il] ];
-          for(Int_t ig=0; ig<ev.ng; ig++)
-            {
-              if(abs(ev.g_id[ig])!=ev.l_id[ selLeptons[il] ]) continue;
-              TLorentzVector glp4;
-              glp4.SetPtEtaPhiM( ev.g_pt[ig], ev.g_eta[ig], ev.g_phi[ig], ev.g_m[ig]);
-              if(glp4.DeltaR( leptons[il] ) > 0.3) continue;
-              tjsev.gl_id[il]=ev.g_id[ig];
-              tjsev.gl_pt[il]=ev.g_pt[ig];
-              tjsev.gl_eta[il]=ev.g_eta[ig];
-              tjsev.gl_phi[il]=ev.g_phi[ig];
-              tjsev.gl_m[il]=ev.g_m[ig];
-            }
         }
       
-      tjsev.nj=bJets.size();
-      for(int ij=0; ij<(int)bJets.size(); ij++)
-        {
-          TString pf(Form("j%d",ij));
-          allPlots[pf+"pt_"+chTag]->Fill(bJets[ij].Pt(),wgt);
-          allPlots[pf+"eta_"+chTag]->Fill(fabs(bJets[ij].Eta()),wgt);
-          tjsev.j_pt[ij]=bJets[ij].Pt();
-          tjsev.j_eta[ij]=bJets[ij].Eta();
-          tjsev.j_phi[ij]=bJets[ij].Phi();
-          tjsev.j_m[ij]=bJets[ij].M();
-          tjsev.gj_flav[ij]=genBJetsFlav[ij];
-          tjsev.gj_hadflav[ij]=genBJetsHadFlav[ij];
-          tjsev.gj_pt[ij]=genBJets[ij].Pt();
-          tjsev.gj_eta[ij]=genBJets[ij].Eta();
-          tjsev.gj_phi[ij]=genBJets[ij].Phi();
-          tjsev.gj_m[ij]=genBJets[ij].M();          
+      //fill jet constituents
+      for (int i = 0; i < tjsev.nj; i++) {
+        int idx = jets[i].oldidx;
+        for (int p = 0; p < ev.npf; p++) {
+          if (ev.pf_j[p] == idx) {
+            TLorentzVector pp4;
+            pp4.SetPtEtaPhiM(ev.pf_pt[p],ev.pf_eta[p],ev.pf_phi[p],ev.pf_m[p]);
+            jets[i].particles.push_back(Particle(pp4, ev.pf_c[p], ev.pf_puppiWgt[p]));
+          }
         }
+      }
       
-      tjsev.nj+=otherjets.size();
-      for(int ij=0; ij<(int)otherjets.size(); ij++)
+      //fill jets (with jet shapes)
+      for(int ij=0; ij<(int)jets.size(); ij++)
         {
-          if(ij+2<6)
-            {
-              TString pf(Form("j%d",ij+2));
-              allPlots[pf+"pt_"+chTag]->Fill(otherjets[ij].Pt(),wgt);
-              allPlots[pf+"eta_"+chTag]->Fill(fabs(otherjets[ij].Eta()),wgt);
-            }
-          tjsev.j_pt[ij+2]=otherjets[ij].Pt();
-          tjsev.j_eta[ij+2]=otherjets[ij].Eta();
-          tjsev.j_phi[ij+2]=otherjets[ij].Phi();
-          tjsev.j_m[ij+2]=otherjets[ij].M();
-          tjsev.gj_flav[ij+2]=genOtherjetsFlav[ij];
-          tjsev.gj_hadflav[ij+2]=genOtherjetsHadFlav[ij];
-          tjsev.gj_pt[ij+2]=genOtherjets[ij].Pt();
-          tjsev.gj_eta[ij+2]=genOtherjets[ij].Eta();
-          tjsev.gj_phi[ij+2]=genOtherjets[ij].Phi();
-          tjsev.gj_m[ij+2]=genOtherjets[ij].M();          
+          tjsev.j_pt[ij]   = jets[ij].p4.Pt();
+          tjsev.j_eta[ij]  = jets[ij].p4.Eta();
+          tjsev.j_phi[ij]  = jets[ij].p4.Phi();
+          tjsev.j_m[ij]    = jets[ij].p4.M(); 
+          tjsev.j_flav[ij] = jets[ij].flav;
+          for (int i1 = 0; i1 < 3; ++i1) for (int i2 = 0; i2 < 3; ++i2) for (int i3 = 0; i3 < 3; ++i3) for (int i4 = 0; i4 < 3; ++i4)
+          tjsev.j_ga[ij][i1][i2][i3][i4] = calcGA(jets[ij], i1, i2, i3, i4);
+          //matching to gen jet
+          
+          for(int ig=0; ig<(int)genJets.size(); ig++) {
+	          if(jets[ij].p4.DeltaR(genJets[ig].p4)>0.4) continue;
+	          tjsev.j_gj[ij] = ig;
+	          tjsev.gj_j[ig] = ij;
+	          break;
+	        }
         }
 
-      tjsev.cat=11;
-      if(chTag=="M") tjsev.cat=13;
-      if(chTag=="MM") tjsev.cat=13*13;
-      if(chTag=="EM") tjsev.cat=11*13;
-      if(chTag=="EE") tjsev.cat=11*11;
       tjsev.nw=1;
       tjsev.weight[0]=wgt;
-      tjsev.nt=0;
       tjsev.met_pt=ev.met_pt[0];
       tjsev.met_phi=ev.met_phi[0];
-      if(ev.ngtop>0)
-        {
-          for(int i=0; i<ev.ngtop; i++)
-            {
-              if(abs(ev.gtop_id[i])!=6) continue;
-              tjsev.t_pt[tjsev.nt]=ev.gtop_pt[i];
-              tjsev.t_eta[tjsev.nt]=ev.gtop_eta[i];
-              tjsev.t_phi[tjsev.nt]=ev.gtop_phi[i];
-              tjsev.t_m[tjsev.nt]=ev.gtop_m[i];
-              tjsev.t_id[tjsev.nt]=ev.gtop_id[i];
-              tjsev.nt++;
-              if(tjsev.nt>4) break;
-            }
-        }
-      */
-      int reco_sel = -1;
-      if (tjsev.gen_sel + reco_sel > -1) outT->Fill();
+      
+      outT->Fill();
     }
   
   //close input file
@@ -581,6 +542,8 @@ double calcGA(Jet jet, int beta, int kappa, int iptcut, int icharge) {
     else if (icharge == 2) sumpt += p.p4.Pt()*p.puppi;
   }
   //std::cout << "sumpt" << beta << kappa << iptcut << icharge << ": " << sumpt << std::endl;
+  //FIXME! jet pt > sum particle pt
+  //if (beta == 0 && kappa == 0 && iptcut == 0 && icharge == 1) std::cout << "jet pt: " << jet.p4.Pt() << ", sum pt: " << sumpt << std::endl;
   
   double ga = 0;
   for (auto p : jet.particles) {
@@ -598,9 +561,6 @@ double calcGA(Jet jet, int beta, int kappa, int iptcut, int icharge) {
 //
 void createTopJetShapeEventTree(TTree *t,TopJetShapeEvent_t &tjsev)
 {
-  //event category
-  t->Branch("cat", &tjsev.cat,"cat/I");
-
   //event weights
   t->Branch("nw",  &tjsev.nw, "nw/I");
   t->Branch("weight",  tjsev.weight, "weight[nw]/F");
@@ -631,6 +591,9 @@ void createTopJetShapeEventTree(TTree *t,TopJetShapeEvent_t &tjsev)
   t->Branch("j_eta", tjsev.j_eta , "j_eta[nj]/F");
   t->Branch("j_phi", tjsev.j_phi , "j_phi[nj]/F");
   t->Branch("j_m",   tjsev.j_m ,   "j_m[nj]/F");
+  t->Branch("j_flav",  tjsev.j_flav ,  "j_flav[nj]/I");
+  t->Branch("j_gj",  tjsev.j_gj ,  "j_gj[nj]/I");
+  t->Branch("j_ga",  tjsev.j_ga ,  "j_ga[nj][3][3][3][3]/F");
   
   //gen jets
   t->Branch("ngj",  &tjsev.ngj, "ngj/I");
@@ -639,31 +602,24 @@ void createTopJetShapeEventTree(TTree *t,TopJetShapeEvent_t &tjsev)
   t->Branch("gj_phi", tjsev.gj_phi , "gj_phi[ngj]/F");
   t->Branch("gj_m",   tjsev.gj_m ,   "gj_m[ngj]/F");
   t->Branch("gj_flav",  tjsev.gj_flav ,  "gj_flav[ngj]/I");
-  t->Branch("gj_hadflav",  tjsev.gj_hadflav ,  "gj_hadflav[ngj]/I");
+  t->Branch("gj_j",  tjsev.gj_j ,  "gj_j[ngj]/I");
   t->Branch("gj_ga",  tjsev.gj_ga ,  "gj_ga[ngj][3][3][3][3]/F");
-
-  //mc truth
-  t->Branch("nt",  &tjsev.nt, "nt/I");
-  t->Branch("t_pt",  tjsev.t_pt ,  "t_pt[nt]/F");
-  t->Branch("t_eta", tjsev.t_eta , "t_eta[nt]/F");
-  t->Branch("t_phi", tjsev.t_phi , "t_phi[nt]/F");
-  t->Branch("t_m",   tjsev.t_m ,   "t_m[nt]/F");
-  t->Branch("t_id",  tjsev.t_id ,  "t_id[nt]/I");
   
   t->Branch("gen_sel", &tjsev.gen_sel ,  "gen_sel/I");
+  t->Branch("reco_sel", &tjsev.reco_sel ,  "reco_sel/I");
 }
 
 //
 void resetTopJetShapeEvent(TopJetShapeEvent_t &tjsev)
 {
-  tjsev.cat=0;   tjsev.nw=0;   tjsev.nl=0;   tjsev.nj=0;   tjsev.ngj=0;   tjsev.ngl=0;   tjsev.nt=0;
-  tjsev.met_pt=0; tjsev.met_phi=0;
+  tjsev.nw=0;   tjsev.nl=0;   tjsev.nj=0;   tjsev.ngj=0;   tjsev.ngl=0;   tjsev.met_pt=0; tjsev.met_phi=0;
   for(int i=0; i<10; i++) tjsev.weight[i]=0;
   for(int i=0; i<5; i++) { tjsev.l_pt[i]=0;   tjsev.l_eta[i]=0;   tjsev.l_phi[i]=0;   tjsev.l_m[i]=0; tjsev.l_id[i]=0; tjsev.gl_pt[i]=0;   tjsev.gl_eta[i]=0;   tjsev.gl_phi[i]=0;   tjsev.gl_m[i]=0; tjsev.gl_id[i]=0; }
   for(int i=0; i<50; i++) {
-    tjsev.j_pt[i]=0;   tjsev.j_eta[i]=0;   tjsev.j_phi[i]=0;   tjsev.j_m[i]=0; tjsev.gj_pt[i]=0;   tjsev.gj_eta[i]=0;   tjsev.gj_phi[i]=0;   tjsev.gj_m[i]=0; tjsev.gj_flav[i]=0; tjsev.gj_hadflav[i]=0;
-    for (int i1 = 0; i1 < 3; ++i1) for (int i2 = 0; i2 < 3; ++i2) for (int i3 = 0; i3 < 3; ++i3) for (int i4 = 0; i4 < 3; ++i4) tjsev.gj_ga[i][i1][i2][i3][i4] = 0;
+    tjsev.j_pt[i]=0;   tjsev.j_eta[i]=0;   tjsev.j_phi[i]=0;   tjsev.j_m[i]=0; tjsev.j_flav[i]=0; tjsev.j_gj[i]=-1;
+    tjsev.gj_pt[i]=0;   tjsev.gj_eta[i]=0;   tjsev.gj_phi[i]=0;   tjsev.gj_m[i]=0; tjsev.gj_flav[i]=0; tjsev.gj_j[i]=-1;
+    for (int i1 = 0; i1 < 3; ++i1) for (int i2 = 0; i2 < 3; ++i2) for (int i3 = 0; i3 < 3; ++i3) for (int i4 = 0; i4 < 3; ++i4) { tjsev.j_ga[i][i1][i2][i3][i4] = 0; tjsev.gj_ga[i][i1][i2][i3][i4] = 0; }
   } 
-  for(int i=0; i<4; i++) { tjsev.t_pt[i]=0;   tjsev.t_eta[i]=0;   tjsev.t_phi[i]=0;   tjsev.t_m[i]=0; tjsev.t_id[i]=0; }
-  tjsev.gen_sel = -1;
+  
+  tjsev.gen_sel = -1; tjsev.reco_sel = -1;
 }
